0f74b788fb9b860e51c79d280236ef6adca355e0
rowan <sk16013@eadevsvcskcorp.onmicrosoft.com>
2024-10-09 05:02:02 +0900

    Area 엔티티 설계

--- src/main/kotlin/com/hsik/smoking/domain/area/Area.kt
+++ src/main/kotlin/com/hsik/smoking/domain/area/Area.kt
@@ -3,8 +3,12 @@ package com.hsik.smoking.domain.area
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
 import java.time.LocalDateTime
 
+/**
+ * 흡연소
+ */
 @Document
 class Area {
 
@@ -12,12 +16,56 @@ class Area {
     var id: ObjectId = ObjectId()
         private set
 
+    /**
+     * 생성 시각
+     */
     var createdAt = LocalDateTime.now()
         private set
 
+    /**
+     * 수정 시각
+     */
     var modifiedAt = LocalDateTime.now()
         private set
 
+    /**
+     * 공공 데이터 포탈에서 데이터를 수집/수정한 일자
+     */
+    var metadataUpdateDate: LocalDate? = null
+
+    /**
+     * 주소
+     */
+    var address: String? = null
+
+    /**
+     * 위도
+     */
     var langitude: Double? = null
+
+    /**
+     * 경도
+     */
     var longitude: Double? = null
+
+    /**
+     * 운영 관리자
+     */
+    var manager: String? = null
+
+    /**
+     * 운영 상태
+     */
+    var status: Status? = null
+
+    /**
+     * 장소에 대한 설명
+     */
+    var description: String? = null
+
+    enum class Status(val desc: String) {
+        OPEN("지도에 노출"),
+        HIDE("지도에 미노출"),
+        CLOSED("폐쇄")
+    }
 }
