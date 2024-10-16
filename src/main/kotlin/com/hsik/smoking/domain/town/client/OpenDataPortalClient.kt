05b58b09a039f0221d1d4a78a892fdaa436ff12d
Hyeonsik Kim <hsik0225@gmail.com>
2024-10-16 23:28:25 +0900

    공공 데이터 API 응답 클래스명 수정 (#12)
    Co-authored-by: rowan <sk16013@eadevsvcskcorp.onmicrosoft.com>

--- src/main/kotlin/com/hsik/smoking/domain/town/client/OpenDataPortalClient.kt
+++ src/main/kotlin/com/hsik/smoking/domain/town/client/OpenDataPortalClient.kt
@@ -1,5 +1,5 @@
 package com.hsik.smoking.domain.town.client
 
 interface OpenDataPortalClient {
-    fun findAllSmokingAreaByDongDaemungu(): List<OpenDataPortalResources.SmokingArea.DongDaeMunGu>
+    fun findAllSmokingAreaInDongdaemunGu(): List<OpenDataPortalResources.SmokingArea.DongdaemunGu>
 }
