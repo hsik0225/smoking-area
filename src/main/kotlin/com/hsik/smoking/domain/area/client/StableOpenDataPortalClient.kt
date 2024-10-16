bd16eefe1bdfc8188e31645e49b86892efc2f3c5
rowan <sk16013@eadevsvcskcorp.onmicrosoft.com>
2024-10-16 18:40:02 +0900

    추가 작업이 필요한 로직을 TODO로 작성

--- src/main/kotlin/com/hsik/smoking/domain/area/client/StableOpenDataPortalClient.kt
+++ src/main/kotlin/com/hsik/smoking/domain/area/client/StableOpenDataPortalClient.kt
@@ -9,6 +9,7 @@ class StableOpenDataPortalClient(
     private val restClient: RestClient,
 ) : OpenDataPortalClient {
     override fun findAllSmokingAreaByDongDaemungu(): List<OpenDataPortalResources.SmokingArea.DongDaeMunGu> {
+        // TODO 실제 URI 작성 및 호출에 사용할 API 인증키 설정 필요
         val uri = "${env.host}/test"
 
         return restClient
