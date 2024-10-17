b8c44563fc28f3cf0c891e33d851315ffca48594
Hyeonsik Kim <hsik0225@gmail.com>
2024-10-17 20:36:49 +0900

    공공 데이터 포탈의 호스트와 더미 ApiKey 작성 (#21)
    * 공공 데이터 호출 URI 작성
    
    * 공공 데이터 포탈의 호스트와 더미 ApiKey 작성
    
    ---------
    
    Co-authored-by: rowan <sk16013@eadevsvcskcorp.onmicrosoft.com>

--- src/main/kotlin/com/hsik/smoking/domain/client/StableOpenDataPortalClient.kt
+++ src/main/kotlin/com/hsik/smoking/domain/client/StableOpenDataPortalClient.kt
@@ -1,5 +1,6 @@
 package com.hsik.smoking.domain.client
 
+import com.hsik.smoking.common.OpenDataPortalReply
 import com.hsik.smoking.config.AppEnvironment
 import com.hsik.smoking.util.RestClient
 import com.hsik.smoking.util.fromJson
@@ -9,11 +10,12 @@ class StableOpenDataPortalClient(
     private val restClient: RestClient,
 ) : OpenDataPortalClient {
     override fun findAllSmokingAreaInDongdaemunGu(): List<OpenDataPortalResources.SmokingArea.DongdaemunGu> {
-        // TODO 실제 URI 작성 및 호출에 사용할 API 인증키 설정 필요
-        val uri = "${env.host}/test"
-
+        // Authorization 헤더에 인증키를 담아 보낼 경우 오픈 데이터 포털에서 인식하지 못하므로 Query로 전달
+        val params = listOf("serviceKey" to env.apiKey)
+        val uri = "${env.host}/api/15070168/v1/uddi:04b20c76-3628-4095-93bb-8e9523a1241a"
         return restClient
-            .get(uri)
-            .fromJson<List<OpenDataPortalResources.SmokingArea.DongdaemunGu>>()
+            .get(uri, null, params)
+            .fromJson<OpenDataPortalReply<OpenDataPortalResources.SmokingArea.DongdaemunGu>>()
+            .data
     }
 }
